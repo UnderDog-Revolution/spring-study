c368a2527ae2ba4191dcf55131c260c5778a91e6
오정빈 <126961816+LeoOH5@users.noreply.github.com>
2024-10-07 18:21:06 +0900

    1주차_3일차_오정빈

---
+++ 오정빈/1주차/3일차/코드/hello_spring/controller/MemberController.java
@@ -0,0 +1,44 @@
+package hello.hello_spring.controller;
+
+
+import hello.hello_spring.domain.Member;
+import hello.hello_spring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
+
+@Controller
+public class MemberController {
+
+    private final MemberService memberService;
+
+    @Autowired
+    public MemberController(MemberService memberService) {
+        this.memberService = memberService;
+    }
+
+    @GetMapping("/members/new")
+    public String createForm() {
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new") // GetMapping은 get 요청을 받을 때 사용 지금은 Post 요청이여서 PostMapping
+    public String create(MemberForm form) {
+        Member member = new Member();
+        member.setName(form.getName());
+
+        memberService.join(member);
+        return "redirect:/";  // 회원가입 끝났으니 홈으로 보내기
+    }
+
+    @GetMapping("/members")
+    public String list(Model model) {
+        List<Member> members = memberService.findMember();
+        model.addAttribute("members", members);
+        return "members/memberList";
+    }
+}
---
+++ 오정빈/1주차/3일차/코드/hello_spring/repository/SpringDataJpaMemberRepository.java
@@ -0,0 +1,13 @@
+package hello.hello_spring.repository;
+
+import hello.hello_spring.domain.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface SpringDataJpaMemberRepository extends JpaRepository<Member,Long>, MemberRepository {
+
+    @Override
+    Optional<Member> findByName(String name);
+
+}
