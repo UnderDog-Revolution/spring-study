363bab8e2a7fc652c4adef4cdc3e7d6fff5191fa
장서연 <151913785+jangse0@users.noreply.github.com>
2024-10-08 18:18:06 +0900

    Add files via upload

---
+++ 장서연/1주차_장서연/2일차/코드/src/main/java/hello/hello_spring/controller/HelloController.java
@@ -0,0 +1,22 @@
+package hello.hello_spring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("hello")
+    public String hello(Model model) {
+        model.addAttribute("data", "spring!!");
+        return "hello";
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam(value = "name", required = false) String name, Model model) {
+        model.addAttribute( "name", name);
+        return "hello-template";
+    }
+}
